1e97eaec38b8162176d8373dfda75e7189c061f9
loloa009 <heloa.galvao@escola.pr.gov.br>
2026-05-11 10:15:32 -0300

    save

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-cconst botoes = document.querySelectorAll(".botao");
+const botoes = document.querySelectorAll(".botao");
 const textos = document.querySelectorAll(".aba-conteudo");
 
 for (let i = 0; i < botoes.length; i++) {
@@ -15,7 +15,7 @@ for (let i = 0; i < botoes.length; i++) {
 }
 
 const contadores = document.querySelectorAll(".contador");
-const tempoObjetivo1 = new Date("2023-10-05T00:00:00");
+const tempoObjetivo1 = new Date("2026-10-05T00:00:00");
 let tempoAtual = new Date();
 
 contadores[0].textContent = tempoObjetivo1 - tempoAtual;
