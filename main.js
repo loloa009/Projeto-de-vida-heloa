469fb8ddaf97bf475e091005159de4a498d1ecd3
loloa009 <heloa.galvao@escola.pr.gov.br>
2026-05-18 08:17:55 -0300

    prazos

--- main.js
+++ main.js
@@ -16,22 +16,42 @@ for (let i = 0; i < botoes.length; i++) {
 
 const contadores = document.querySelectorAll(".contador");
 const tempoObjetivo1 = new Date("2026-10-05T00:00:00");
-let tempoAtual = new Date();
+const tempoObjetivo2 = new Date("2026-12-05T00:00:00");
+const tempoObjetivo3 = new Date("2026-12-30T00:00:00");
+const tempoObjetivo4 = new Date("2027-10-30T00:00:00");
 
-contadores[0].textContent = calculaTempo(tempoObjetivo1);
+const tempo = [tempoObjetivo1,tempoObjetivo2,tempoObjetivo3,tempoObjetivo4];
+
 
 function calculaTempo(tempoObjetivo) {
- let tempoAtual = new Date();
- let tempoFinal = tempoObjetivo - tempoAtual;
- let segundos = Math.floor(tempoFinal/1000);
- let minutos = Math.floor(segundos/60);
- let horas = Math.floor(minutos/60);
- let dias = Math.floor(horas/24);
+    let tempoAtual = new Date();
+    let tempoFinal = tempoObjetivo - tempoAtual;
+    let segundos = Math.floor(tempoFinal / 1000);
+ let minutos = Math.floor(segundos / 60);
+ let horas = Math.floor(minutos / 60);
+ let dias = Math.floor(horas / 24);
+ 
+ segundos %= 60;
+ minutos %= 60;
+ dias %= 24;
+ if (tempoFinal > 0){
+     return dias + "dias" + horas + "horas" + minutos + "minutos" + segundos + "segunodos"
+ } else{
+    return "Prazo finalizado";
+ }
 
-segundos %= 60;
-minutos %= 60;
-dias %= 24;
-
- return dias + "dias" + horas + "horas" + minutos + "minutos" + segundos + "segunodos"
-
+ 
 }
+
+function atualizaCronometro(){
+    for-(let i=0; i<contadores.length;i++){
+            contadores[i].textContent = calculaTempo(tempos[i]);
+    
+        }
+}
+function começaConometro(){
+    atualizaCronometro();
+    setInterval(atualizaCronometro;1000);
+}
+
+começaConometro();
